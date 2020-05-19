19ddfdfdfe21b697b8e0a6f1d763d289f0c2d25a
Alex P <alexeek@protonmail.com>
2020-05-19 07:17:02 +0100

    fix: set the instantiated Channel in Channels

--- internal/http/ws/change_listen.go
+++ internal/http/ws/change_listen.go
@@ -28,6 +28,7 @@ func handleChangeListen(c *Client, msg Message) {
 	if !ok {
 		var err error
 		channel, err = NewChannel(strID)
+		C.Channels[strID] = channel
 
 		if err != nil {
 			marshalled, err := json.Marshal(Message{
