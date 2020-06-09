49bd45bfbfd7a9fc0e590e40e7eac49767098499
Alex P <alexeek@protonmail.com>
2020-06-09 19:32:17 +0100

    fix: add JSON tags to schemas and omit empty vals

--- internal/db/schemas.go
+++ internal/db/schemas.go
@@ -5,26 +5,26 @@ import (
 )
 
 type Channel struct {
-	ID      string          `bson:"_id"`
-	Name    string          `bson:"name"`
-	Date    time.Time       `bson:"date"`
-	Pending map[string]bool `bson:"pending"`
-	Users   map[string]bool `bson:"users"`
-	Parent  string          `bson:"parent"`
+	ID      string          `bson:"_id" json:"id,omitempty"`
+	Name    string          `bson:"name" json:"name,omitempty"`
+	Date    time.Time       `bson:"date" json:"date,omitempty"`
+	Pending map[string]bool `bson:"pending" json:"pending,omitempty"`
+	Users   map[string]bool `bson:"users" json:"users,omitempty"`
+	Parent  string          `bson:"parent" json:"parent,omitempty"`
 }
 
 type User struct {
-	ID        string    `bson:"_id"`
-	Date      time.Time `bson:"date"`
-	Username  string    `bson:"username"`
-	Password  string    `bson:"password"`
-	PublicKey string    `bson:"public_key"`
+	ID        string    `bson:"_id" json:"id,omitempty"`
+	Date      time.Time `bson:"date" json:"date,omitempty"`
+	Username  string    `bson:"username" json:"username,omitempty"`
+	Password  string    `bson:"password" json:"password,omitempty"`
+	PublicKey string    `bson:"public_key" json:"public_key,omitempty"`
 }
 
 type Message struct {
-	ID        string    `bson:"_id"`
-	ChannelID string    `bson:"channel_id"`
-	Date      time.Time `bson:"date"`
-	Data      string    `bson:"data"`
-	SignedBy  string    `bson:"signed_by"`
+	ID        string    `bson:"_id" json:"id,omitempty"`
+	ChannelID string    `bson:"channel_id" json:"channel_id,omitempty"`
+	Date      time.Time `bson:"date" json:"date,omitempty"`
+	Data      string    `bson:"data" json:"data,omitempty"`
+	SignedBy  string    `bson:"signed_by" json:"signed_by,omitempty"`
 }
